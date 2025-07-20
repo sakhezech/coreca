b734a86b5948bbd1a8b898e5f33de9210e90314b
sakhezech <contact@sakhezech.ru>
2025-07-20 21:02:29 +0300

    feat: websocket server lifespan

--- src/coreca/lifespans.py
+++ src/coreca/lifespans.py
@@ -2,12 +2,15 @@ import contextlib
 import functools
 import http.server
 import threading
-from collections.abc import Generator
+import time
+from collections.abc import Callable, Generator
 from pathlib import Path
 from typing import Literal
 
 import watchdog.events
 import watchdog.observers
+import websockets.exceptions
+import websockets.sync.server
 
 from . import Core, Signal
 
@@ -66,3 +69,37 @@ def serve(
     yield
     httpd.shutdown()
     httpd_thread.join()
+
+
+@contextlib.contextmanager
+def websocket[T](
+    core: 'Core[T]',
+    host_port: tuple[str, int],
+    handler: Callable[['Core[T]', 'Signal'], str],
+) -> Generator[None]:
+    _msg = None
+
+    def websocket_handler(ws: websockets.sync.server.ServerConnection) -> None:
+        nonlocal _msg
+        try:
+            while True:
+                if _msg:
+                    ws.send(_msg)
+                    _msg = None
+                time.sleep(0.1)
+        except websockets.exceptions.ConnectionClosed:
+            pass
+
+    def signal_handler(core: 'Core[T]', signal: 'Signal') -> None:
+        nonlocal _msg
+        _msg = handler(core, signal)
+
+    wsd = websockets.sync.server.serve(websocket_handler, *host_port)
+    wsd_thread = threading.Thread(target=wsd.serve_forever)
+    wsd_thread.start()
+    # HACK: signal_handler is not guarantied to be mutable
+    core.signal_handlers.append(signal_handler)
+    yield
+    core.signal_handlers.remove(signal_handler)
+    wsd.shutdown()
+    wsd_thread.join()
